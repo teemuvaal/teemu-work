f0dd50545a790dd980ca0519aaf635d74412684e
Teemu Vaalasmaa <flipsuk@gmail.com>
2024-08-01 20:57:12 +0300

    fixes

--- app/components/profile.jsx
+++ app/components/profile.jsx
@@ -2,7 +2,11 @@ export default function Profile () {
     return (
         <div>
         <h1 className='text-md'>About</h1>
-        <p className='font-light text-sm text-gray-700 py-1 px-2'>My lifelong passion for technology led me to pursue a degree in education, seeking to explore the intricate relationship between humans, information, technology, and learning. This unique academic background has propelled me into the world of online education.</p>
+        <p className='font-light text-sm text-gray-700 py-1 px-2'>
+            I build things for the web for myself. Also expert in anything 
+            that comes to online learning. I enjoy data analysis and visualization and 
+            have been doing so in the field of learning analytics. I like the stuff between humans and technology.
+        </p>
         </div>
     )
 }

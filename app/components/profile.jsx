f90b27c43f63c66141e3465f545d159d27f7b4f7
Teemu Vaalasmaa <flipsuk@gmail.com>
2024-08-01 23:10:41 +0300

    Adjust profile

--- app/components/profile.jsx
+++ app/components/profile.jsx
@@ -4,8 +4,9 @@ export default function Profile () {
         className="py-2">
         <h1 className='text-md'>About</h1>
         <p className='font-light text-sm text-gray-700 py-1 px-2'>
-            I build things for the web for myself, but also fiddle with the front-end of <a className="text-gray-500 underline" href="https://claned.com" target="_blank">CLANED</a>. Also expert in anything 
-            that comes to online learning. Learning Analytics is my jam but into Data Analytics in general and especially AI. I like the stuff between humans and technology.
+            I'm an adult educator who leaped to the dark side - startups.
+            I build things for the web for myself, but also fiddle with the front-end of <a className="text-gray-500 underline" href="https://claned.com" target="_blank">CLANED</a>. Expert in anything 
+            that comes to online learning. Learning Analytics is my jam but I'm into Data Analytics in general and especially if you can enhance it with AI. I like the stuff between humans and technology.
         </p>
         </div>
     )

8e66a2628ba6c08b85b64a0c940ece6a9ea8d356
Teemu Vaalasmaa <flipsuk@gmail.com>
2024-08-02 14:20:28 +0300

    fix responsiveness

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -7,7 +7,7 @@ export default function Projects() {
         className="py-2"
         >
             <h2>Projects, etc.</h2>
-            <div className="flex flex-row gap-2 p-2">
+            <div className="flex flex-col lg:flex-row gap-2 p-2 md:flex-col">
                 {config.Projects.map((project, index) => (
                     <ProjectCard key={index} title={project.title} description={project.description} href={project.href} cover={project.cover}/>
                 ))}

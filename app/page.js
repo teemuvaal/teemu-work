849d8a16545028c1c1afd7942cfe6b42d235ca8d
Teemu Vaalasmaa <flipsuk@gmail.com>
2024-10-29 17:42:36 +0200

    layout adjust

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ import CopyHere from "./components/CopyHere";
 export default function Home() {
   return (
   <div className='flex items-center justify-center mt-10'>
-  <div className='w-2/3 md:w-2/3 lg:w-2/5'>
+  <div className='mx-4 w-full md:w-2/3 lg:w-2/5 lg:mx-0 md:mx-10'>
     <Header />
     <Divider />
     <Profile />
